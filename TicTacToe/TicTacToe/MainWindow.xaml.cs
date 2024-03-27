704606a015f903663933d0fd3a64ebe3a8f4ee1e
akoljtsov <163341667+akoljtsov@users.noreply.github.com>
2024-03-27 22:07:12 +0200

    Add files via upload

---
+++ TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TicTacToe
+{
+    public partial class TicTacToeWindow : Window
+    {
+        private bool xTurn = true; // Черга хрестиків (true - хрестики, false - нулики)
+        private Button[,] buttons; // Масив кнопок для керування грою
+        private const int BoardSize = 3; // Розмір ігрового поля
+
+        public TicTacToeWindow()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            buttons = new Button[BoardSize, BoardSize];
+            for (int i = 0; i < BoardSize; i++)
+            {
+                for (int j = 0; j < BoardSize; j++)
+                {
+                    Button button = new Button
+                    {
+                        Content = " ",
+                        FontSize = 24,
+                        Tag = new Tuple<int, int>(i, j)
+                    };
+                    button.Click += Button_Click;
+                    TicTacToeGrid.Children.Add(button);
+                    Grid.SetRow(button, i);
+                    Grid.SetColumn(button, j);
+                    buttons[i, j] = button;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Tuple<int, int> position = (Tuple<int, int>)button.Tag;
+            if (button.Content.ToString() == " " && !IsGameOver())
+            {
+                button.Content = xTurn ? "X" : "O";
+                xTurn = !xTurn;
+                if (IsGameOver())
+                {
+                    string winner = xTurn ? "O" : "X";
+                    MessageBox.Show($"Game over! Winner: {winner}");
+                }
+            }
+        }
+
+        private bool IsGameOver()
+        {
+            for (int i = 0; i < BoardSize; i++)
+            {
+                if (AreEqual(buttons[i, 0], buttons[i, 1], buttons[i, 2]))
+                    return true;
+
+                if (AreEqual(buttons[0, i], buttons[1, i], buttons[2, i]))
+                    return true;
+            }
+
+            if (AreEqual(buttons[0, 0], buttons[1, 1], buttons[2, 2]) ||
+                AreEqual(buttons[0, 2], buttons[1, 1], buttons[2, 0]))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool AreEqual(Button b1, Button b2, Button b3)
+        {
+            return b1.Content.ToString() != " " && b1.Content == b2.Content && b2.Content == b3.Content;
+        }
+
+        private void ResetGame_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Button button in TicTacToeGrid.Children)
+            {
+                button.Content = " ";
+            }
+            xTurn = true;
+        }
+    }
+}
---
+++ calculator/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp
+{
+    public partial class CalculatorWindow : Window
+    {
+        private string input = string.Empty;
+
+        public string Input
+        {
+            get { return input; }
+            set
+            {
+                input = value;
+                InputBox.Text = input;
+            }
+        }
+
+        public CalculatorWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            string number = ((Button)sender).Content.ToString();
+            Input += number;
+        }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            string operation = ((Button)sender).Content.ToString();
+            Input += " " + operation + " ";
+        }
+
+        private void EqualsButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var dataTable = new System.Data.DataTable();
+                var result = dataTable.Compute(Input, "");
+                Input = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            Input = string.Empty;
+        }
+
+        private void ClearEntryButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Input))
+            {
+                Input = Input.Substring(0, Input.Length - 1);
+            }
+        }
+
+        private void BackspaceButton_Click(object sender, RoutedEventArgs e)
+        {
+            Input = string.Empty;
+        }
+
+        private void NegateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Input) && Input[0] == '-')
+            {
+                Input = Input.Substring(1);
+            }
+            else
+            {
+                Input = "-" + Input;
+            }
+        }
+    }
+}
